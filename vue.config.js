da2f997e2af40072a0f041527bbb8dd6f2b37516
小白白 <2148135358@qq.com>
2025-08-27 12:37:43 +0800

    feat: set page title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './',
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        // 在这里修改标题
+        args[0].title = '智慧商城' // 或者您想要的任何名字
+        return args
+      })
+  }
 })
